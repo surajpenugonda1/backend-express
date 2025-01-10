30f64e91b25252427886cfe2405a28ffb5f6bf31
surajrockzz <penugondasuraj@gmail.com>
2025-01-10 16:17:41 -0600

    added basic auth handler and error handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express();
 
-app.use("/", (req, res) => {
+// custom basic  auth handler for all routes 
+app.use("/", (req, res, next) => {
+    const token = 'xyz';
+    const authHeader = 'xyz';
+    if (token === authHeader) {
+        next();
+    } else {
+        res.status(401).send('Unauthorized');
+    }
+});
+
+app.get("/getdata", (req, res) => {
     res.send('Hello World');
 })
+
+// 
+app.get("/getdataerror", (req, res) => {
+    throw new Error('This is a forced error');
+    res.send('Hello World');
+})
+
+app.use("/", (req, res) => {
+    res.send('Inital Page for all');
+});
+
+// for sure write 4 parameters for error handling 
+// custom error handler for all routes 
+app.use("/", (err, req, res, next) => {
+    res.status(500).send('Internal Server Error');
+});
 
 
 app.listen(7080, () => {
